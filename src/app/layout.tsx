ea47bf4a3e8dfee45a68d840e440dc4490fd02cf
CedricRandriamanjaka <randriamanjakacedric@gmail.com>
2026-02-19 23:16:50 +0300

    ajout google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Urgence Plomberie à proximité | Monsieur Boris",
@@ -12,7 +13,21 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
-      <body>{children}</body>
+      <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16981022545"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16981022545');
+          `}
+        </Script>
+        {children}
+      </body>
     </html>
   );
 }
